184d581c128944099402bdf0c8b868e73bfdb697
yurkiss <terranet23@gmail.com>
2019-04-09 01:36:37 +0300

    add loginWindow

--- WpfLoginLocalize/LoginWindow.xaml.cs
+++ WpfLoginLocalize/LoginWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfLoginLocalize
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для LoginWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            //MessageBox.Show("Load");
-            LoginWindow lw = new LoginWindow();
-            lw.ShowDialog();
         }
     }
 }
