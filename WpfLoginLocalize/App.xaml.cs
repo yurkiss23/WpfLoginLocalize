33c50d7c7d8a5ed0d87665ce4b2f1fff5d4a27c6
work <work@TEST>
2019-04-11 16:40:04 +0300

    add localization

--- WpfLoginLocalize/App.xaml.cs
+++ WpfLoginLocalize/App.xaml.cs
@@ -13,5 +13,10 @@ namespace WpfLoginLocalize
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            //System.Threading.Thread.CurrentThread.CurrentUICulture =
+            //    new System.Globalization.CultureInfo("uk");
+        }
     }
 }
